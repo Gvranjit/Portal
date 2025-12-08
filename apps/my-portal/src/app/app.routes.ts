62c609bc6153a65f90d090a5b05fcab775525d0d
Gaurav Ranjit <gvranjit@gmail.com>
2025-12-07 20:27:17 -0500

    Remove NX homepage. Add home page test file

--- apps/my-portal/src/app/app.routes.ts
+++ apps/my-portal/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Route } from '@angular/router';
+import { Home } from './pages/home';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'home',
+    component: Home,
+  },
+];
