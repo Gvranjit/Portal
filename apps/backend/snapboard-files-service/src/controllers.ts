5f83f3de1de90a21e4e82959179f5a1669f2f5fd
Gaurav Ranjit <gvranjit@gmail.com>
2025-12-14 19:41:37 -0500

    Update prisma DB model (primsa migrate cli command used)

--- apps/backend/snapboard-files-service/src/controllers.ts
+++ apps/backend/snapboard-files-service/src/controllers.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from 'express';
 import { env } from './config.env';
+import { prisma } from './client';
 interface MulterRequest extends Request {
   file: Express.Multer.File;
 }
@@ -12,6 +13,10 @@ export const uploadController = (
     if (!req.file) {
       return res.status(400).send({ message: 'No file uploaded' });
     }
+    // // save the metadata for the uploaded file in database
+    // prisma.snap.create({
+    //   data: {},
+    // });
 
     res.send({
       message: 'File uploaded successfully',
--- apps/backend/snapboard-files-service/src/main.ts
+++ apps/backend/snapboard-files-service/src/main.ts
@@ -5,6 +5,25 @@ import { uploadFolder } from './storage.config';
 import morgan from 'morgan';
 const app = express();
 
+//create a user in database if not exists
+// Anonymous user with id 'anon' is used for all uploads
+import { prisma } from './client';
+
+async function ensureAnonymousUser() {
+  const anonUser = await prisma.user.findUnique({
+    where: { id: 1 },
+  });
+  if (!anonUser) {
+    await prisma.user.create({
+      data: {
+        name: 'Anonymous',
+        email: 'anonymous@gauravranjit.com',
+      },
+    });
+    console.log('Created anonymous user with id 1 and name Anonymous');
+  }
+}
+
 app.use(morgan('dev'));
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/i', express.static(uploadFolder));
@@ -17,5 +36,7 @@ app.use((error: Error, req: Request, res: Response, next: NextFunction) => {
 const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
+  ensureAnonymousUser();
+  console.log('Ensured anonymous user exists');
 });
 server.on('error', console.error);
